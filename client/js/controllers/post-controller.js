9dc2957998ead33793a738a89dbd8e5537b0fbcb
Michael Novén <michael.f.noven@gmail.com>
2016-03-24 11:51:13 +0800

    search field filters on everything. closes #1

--- client/js/controllers/post-controller.js
+++ client/js/controllers/post-controller.js
@@ -2,9 +2,7 @@
 
 angular.module('quoraApp')
 .controller('PostsCtrl', [ '$scope', '$stateParams', 'posts', function($scope, $stateParams, posts){
-
 	
-
 	$scope.post = posts.posts[$stateParams.id];
 
 	$scope.addComment = function(){
--- client/js/services/post-service.js
+++ client/js/services/post-service.js
@@ -20,10 +20,22 @@ angular.module('quoraApp')
 								 	]},
 								 {
 								  title: 'Why is nobody asking questions during lecture?', 
-								  category: 'UTown',
+								  category: 'Lectures',
 								  author: 'User2',
 								  views: 25,
-								  desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel scelerisque quam. Pellentesque ut mattis tellus. Donec maximus elementum nibh eget gravida. In sed leo a lectus suscipit porta. Fusce ornare sem vel sem viverra tempor. Aenean et tempus sapien. Donec sit amet mollis nibh. Suspendisse interdum, ipsum a maximus mattis, ipsum nibh viverra ipsum, eget facilisis enim mauris vitae urna. Mauris vulputate libero sed dapibus tristique. Nullam at ante a nisi porttitor rhoncus.',
+								  desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.In sed leo a lectus suscipit porta. Suspendisse interdum, ipsum a maximus mattis, ipsum nibh viverra ipsum, eget facilisis enim mauris vitae urna. Mauris vulputate libero sed dapibus tristique. Nullam at ante a nisi porttitor rhoncus.',
+									upvotes:0,
+									comments: [
+								 		{author:'Bob', body:'Some comment content', upvotes:0},
+								 		{author:'Eric', body:'Some comment content2222', upvotes:0}
+								 	]
+								},
+								{
+								  title: 'When does starbucks close on public holidays?', 
+								  category: 'UTown',
+								  author: 'Steven',
+								  views: 25,
+								  desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel scelerisque quam. Pellentesque ut mattis tellus.  Aliquam vel scelerisque quam. Pellentesque ut mattis tellus.',
 									upvotes:0,
 									comments: [
 								 		{author:'Bob', body:'Some comment content', upvotes:0},
