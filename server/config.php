9ce32d38965f48454e671279d58775392a4f26d5
Shaun Thium <shaun.thium@gmail.com>
2016-03-25 22:22:32 +0800

    Create config.php with required database settings.

---
+++ server/config.php
@@ -0,0 +1,7 @@
+<?php
+  // Change the below to your own personal settings for local testing
+  DEFINE('DB_HOST', 'localhost');
+  DEFINE('DB_USERNAME', 'root');
+  DEFINE('DB_PASSWORD', 'nusanswersrws');
+  DEFINE('DB_DATABASE', 'NUSAnswers');
+ ?>
