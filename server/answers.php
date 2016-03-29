e5cec1229719319daa8eb057eb15117ae125c988
wireless90 <wireless-@hotmail.com>
2016-03-29 15:43:04 +0800

    Fixed Merge Conflict :dancer:

--- server/answers.php
+++ server/answers.php
@@ -6,15 +6,10 @@
 		$queue_id = $_POST["qid"];
 	
 	global $db; // refer to the global variable 'db'
-<<<<<<< HEAD
 	
-	if($cmd == "getanswers" and isset($_POST["qid"]))
-=======
-//	$cmd = "getanswers";
-//	$queue_id = 1;
+
 	
 	if($cmd == "getanswers" and $queue_id >=0)
->>>>>>> origin/razali
 	{
 		/*******************Query Question table ***************************/
 		$query = "select Questions.*, group_concat(Tags.content) as tags from Questions inner join Questions_Tags on Questions.id = Questions_Tags.question_id inner join Tags on Questions_Tags.tag_id = Tags.id where Questions.id = " . $queue_id . " group by Questions.id;";
@@ -26,14 +21,10 @@
 		while ($r = mysqli_fetch_row($res)) { // important command
 			$a =  $r;
 		}
-<<<<<<< HEAD
+
 		$questionResult = array("question_id" =>$a[0], "user_id"=>$a[1], "title"=>$a[2], "content"=>$a[3], "score"=>$a[4], "view_count"=>$a[5], "created_at"=>$a[6], "updated_at"=>$a[7], "tags"=>$a[8]);
+
 		
-=======
-
-		$questionResult = array("question_id" =>$a[0], "user_id"=>$a[1], "title"=>$a[2], "content"=>$a[3], "score"=>$a[4], "view_count"=>$a[5], "created_at"=>$a[6], "updated_at"=>$a[7], "tags"=>$a[8]);
-
->>>>>>> origin/razali
 		
 		/*******************Query Comments table ***************************/
 		$query = "select Comments.id as comments_id, Users.id as user_id, Comments.content, Comments.created_at, Comments.updated_at from Comments inner join Users on Users.id = Comments.user_id  where Comments.question_id = ". $queue_id;
@@ -50,7 +41,7 @@
 		
 		
 		
-<<<<<<< HEAD
+
 		$query = "select Answers.id as answers_id, Answers.user_id, Answers.content, Answers.score, Answers.created_at, Answers.updated_at, Answers.chosen from Answers where question_id = ". $queue_id;
 		$res = $db->query($query); 
 		
@@ -60,35 +51,15 @@
 			array_push($answersResult,array("answers_id"=>$r[0],"user_id"=>$r[1],"content"=>$r[2],"score"=>$r[3],"created_at"=>$r[4],"updated_at"=>$r[5], "chosen"=>$r[6]));
 		}
 		$finalOutput = array("q"=>$questionResult, "c"=>$commentsResult, "a"=>$answersResult);
-		//$finalOutput = array("q"=>$questionResult);
-
-		echo json_encode($finalOutput);
-		
-=======
-		$query = "select Answers.id as answers_id, Answers.user_id, Answers.content, Answers.score, Answers.created_at, Answers.updated_at from Answers where question_id = ". $queue_id;
-		$res = $db->query($query); 
-		
-		$answersResult = array();
-		//| answers_id | user_id | content| score | created_at| updated_at|
-		while ($r = mysqli_fetch_row($res)) { // important command
-			array_push($answersResult,array("answers_id"=>$r[0],"user_id"=>$r[1],"content"=>$r[2],"score"=>$r[3],"created_at"=>$r[4],"updated_at"=>$r[5]));
-		}
 	
 		
 		$finalOutput = array("q"=>$questionResult, "c"=>$commentsResult, "a"=>$answersResult);
 		echo json_encode($finalOutput);
->>>>>>> origin/razali
+
 	}
 
 	
 
 	
-	
-<<<<<<< HEAD
-	function GetInput($cmd)
-	{
-		
-	}
-=======
->>>>>>> origin/razali
+
 ?>
