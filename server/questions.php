7751309a82627cd58c00b37367261371bfc01b49
tangwr <firebolt2000@hotmail.com>
2016-03-30 17:31:49 +0800

    simple modification

--- server/questions.php
+++ server/questions.php
@@ -1,7 +1,7 @@
 <?php require_once 'connect.php';
 	
 	///Get all Title from "Questions" database
-	if($_GET["cmd"] == "title"){
+	if($_POST["cmd"] == "title"){
 		$query = "SELECT title FROM Questions";
 		$result = $db->query($query);
 		$title_array = array();
@@ -12,7 +12,7 @@
 	}
 
 	//Get Trending Post Top 10
-	if($_GET["cmd"] == "trending"){
+	if($_POST["cmd"] == "trending"){
 		$query = "SELECT * FROM Questions ORDER BY score DESC LIMIT 10";
 		$result = $db->query($query);
 		$post_array = array();
