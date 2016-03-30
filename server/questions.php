7a112a77e1fc4e01d41c4f55a02ff81e7e11b0e1
tangwr <firebolt2000@hotmail.com>
2016-03-30 23:02:27 +0800

    update to question php, insert question added

--- server/questions.php
+++ server/questions.php
@@ -4,6 +4,27 @@
 		$cmd = $_POST["cmd"];
 	}
 	
+	//Submit new questions
+	if($cmd == "new_qn"){
+		$user_id= $_POST["user_id"];
+		$title = $_POST["title"];
+		$content= $_POST["content"];
+
+		if(empty($title) || empty($content)){
+			exit("Empty title or content");
+		}
+
+		$query = "INSERT INTO Questions(user_id, title, content, score, view_count) VALUES(".$user_id.",'".$title."','".$content."', 0, 0)";
+		if($db->query($query)){
+			echo "Question Inserted";
+		}else{
+			echo "Fail to insert question";
+		}
+
+
+	}
+
+
 	///Get all Title from "Questions" database
 	if($cmd == "title"){
 		$query = "SELECT title FROM Questions";
@@ -15,9 +36,9 @@
 		echo json_encode($title_array);		
 	}
 
-	//Get Trending Post Top 10
+	//Get Trending post. The post is sorted in descending order of the Score of each post
 	if($cmd == "trending"){
-		$query = "SELECT * FROM Questions ORDER BY score DESC LIMIT 10";
+		$query = "SELECT * FROM Questions ORDER BY score DESC";
 		$result = $db->query($query);
 		$post_array = array();
 		while ($post = mysqli_fetch_array($result)){
@@ -38,4 +59,49 @@
 		}
 		echo json_encode($post_array);		
 	}
+
+	/* 
+	For testing purpose only
+	Update 'score' of user using 'id'
+	*/
+	if($cmd == "update_score"){
+		$id= $_POST["id"];
+		$score = $_POST["score"];
+		$query = "UPDATE Questions SET score=". $score . " WHERE id=" . $id;
+		if($db->query($query)){
+			echo "Score Updated";
+		}else{
+			echo "Fail to update score";
+		}
+	}
+
+	/* 
+	For testing purpose only
+	Update 'view_count' of user using 'id'
+	*/
+	if($cmd == "update_view"){
+		$id= $_POST["id"];
+		$view_count = $_POST["view_count"];
+		$query = "UPDATE Questions SET view_count=". $view_count . " WHERE id=" . $id;
+		if($db->query($query)){
+			echo "View_Count Updated";
+		}else{
+			echo "Fail to update view_count";
+		}
+	}
+
+	/* 
+	For testing purpose only
+	Delete row from table
+	*/
+	if($cmd == "delete"){
+		$id= $_POST["id"];
+		$query = "DELETE FROM Questions WHERE id=" . $id;
+		if($db->query($query)){
+			echo "Row with id='" . $id . "' had been deleted";
+		}else{
+			echo "Fail to delete row with id='" . $id . "'";
+		}
+	}
+
 ?>
