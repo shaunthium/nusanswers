bd3e7a30e17e52b1ce1acab443db6cbb4c4b3c0d
Shaun Thium <shaun.thium@gmail.com>
2016-03-30 14:57:17 +0800

    Move users.php into /server/users and rename to main.php. Write function for getting of a single user in post_functions.php

---
+++ server/users/post_functions.php
@@ -0,0 +1,26 @@
+<?php
+  require_once('../connect.php');
+
+  function get_user($user_id) {
+    global $db;
+    $query = "SELECT * FROM Users WHERE id=" . $user_id;
+
+    $sql_result = $db->query($query);
+    $row = mysqli_fetch_row($sql_result);
+    if (!is_null($row)) {
+      $result = array();
+      $result["id"] = $row[0];
+      $result["first_name"] = $row[1];
+      $result["last_name"] = $row[2];
+      $result["email"] = $row[3];
+      $result["role"] = $row[5];
+      $result["score"] = $row[6];
+      $result["created_at"] = $row[7];
+      $result["updated_at"] = $row[8];
+
+      return $result;
+    } else {
+      return false;
+    }
+  }
+ ?>
