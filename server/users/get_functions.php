04d140545f4bd454d0642186d59fd73190b1dd64
Shaun Thium <shaun.thium@gmail.com>
2016-03-30 23:41:53 +0800

    Implement functionality to get all users' id, first name and last name. To be used in search bar by front-end.

--- server/users/get_functions.php
+++ server/users/get_functions.php
@@ -1,6 +1,6 @@
 <?php
   require_once('../connect.php');
-  
+
   function get_all_users() {
     global $db;
     $query = "SELECT * FROM Users";
@@ -17,6 +17,23 @@
       $user["score"] = $row[6];
       $user["created_at"] = $row[7];
       $user["updated_at"] = $row[8];
+      // Push user into array
+      $result[] = $user;
+    }
+    return $result;
+  }
+
+  function get_users_for_search_results() {
+    global $db;
+    $query = "SELECT id, first_name, last_name FROM Users";
+
+    $sql_result = $db->query($query);
+    $result = array();
+    while (($row = mysqli_fetch_row($sql_result)) != null) {
+      $user = array();
+      $user["id"] = $row[0];
+      $user["first_name"] = $row[1];
+      $user["last_name"] = $row[2];
       // Push user into array
       $result[] = $user;
     }
