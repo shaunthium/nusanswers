26dc78d8214a9f79ab0b650447defc0dc853ccee
wireless90 <wireless-@hotmail.com>
2016-03-31 15:06:39 +0800

    Added upvote/downvote/deleteanswer

--- server/answers.php
+++ server/answers.php
@@ -6,53 +6,46 @@
 		$question_id = $_POST["question_id"];
 	if(isset($_POST["answer_id"]))
 		$answer_id = $_POST["answer_id"];
+	if(isset($_POST["user_id"]))
+		$user_id = $_POST["user_id"];
 		
 	
-	global $db; // refer to the global variable 'db'
+	global $db; 
 	
+	
+	/*
+	* Gets details of Questions, Comments and Answers
+	* 
+	* 
+	*/
 	if($cmd == "getanswers" and $question_id >=0)
 	{
 		/*******************Query Question table ***************************/
 		$query = "select Questions.*, group_concat(Tags.content) as tags from Questions inner join Questions_Tags on Questions.id = Questions_Tags.question_id inner join Tags on Questions_Tags.tag_id = Tags.id where Questions.id = " . $question_id . " group by Questions.id;";
-		
 		$res = $db->query($query); 
-		 //| question_id | user_id | title| content| score | view_count | created_at| updated_at| tags|
-		 
-		$a;
-		while ($r = mysqli_fetch_row($res)) { // important command
-			$a =  $r;
-		}
-
-		$questionResult = array("question_id" =>$a[0], "user_id"=>$a[1], "title"=>$a[2], "content"=>$a[3], "score"=>$a[4], "view_count"=>$a[5], "created_at"=>$a[6], "updated_at"=>$a[7], "tags"=>$a[8]);
-
-		
+		//| question_id | user_id | title| content| score | view_count | created_at| updated_at| tags|
+		$questionResult = array();
+		$questionResult = mysqli_fetch_assoc($res);
+    
 		
 		/*******************Query Comments table ***************************/
-		$query = "select Comments.id as comments_id, Users.id as user_id, Comments.content, Comments.created_at, Comments.updated_at from Comments inner join Users on Users.id = Comments.user_id  where Comments.question_id = ". $question_id;
+		$query = "select Comments.id, Users.id as user_id, Comments.content, Comments.created_at, Comments.updated_at from Comments inner join Users on Users.id = Comments.user_id  where Comments.question_id = ". $question_id;
 		$res = $db->query($query); 
-		
 		 $commentsResult = array();
 		//| comments_id |  users_id|content| created_at| updated_at |
-		while ($r = mysqli_fetch_row($res)) { // important command
-			$a =  $r;
-			array_push($commentsResult, array("comments_id"=>$a[0], "user_id"=>$a[1],"content"=>$a[2],"created_at"=>$a[3],"updated_at"=>$a[4]));
+		while($r = mysqli_fetch_assoc($res)){
+			$commentsResult[] = $r;
 		}
-		
-		
-		
-		
-		
-
+    
+  
+		/*******************Query Answers table ***************************/
 		$query = "select Answers.id as answers_id, Answers.user_id, Answers.content, Answers.score, Answers.created_at, Answers.updated_at, Answers.chosen from Answers where question_id = ". $question_id;
 		$res = $db->query($query); 
-		
 		$answersResult = array();
 		//| answers_id | user_id | content| score | created_at| updated_at| chosen
-		while ($r = mysqli_fetch_row($res)) { // important command
-			array_push($answersResult,array("answers_id"=>$r[0],"user_id"=>$r[1],"content"=>$r[2],"score"=>$r[3],"created_at"=>$r[4],"updated_at"=>$r[5], "chosen"=>$r[6]));
+		while($r = mysqli_fetch_assoc($res)){
+			$answersResult[] = $r;
 		}
-		$finalOutput = array("q"=>$questionResult, "c"=>$commentsResult, "a"=>$answersResult);
-	
 		
 		$finalOutput = array("q"=>$questionResult, "c"=>$commentsResult, "a"=>$answersResult);
 		echo json_encode($finalOutput);
@@ -65,4 +58,20 @@
 		$res = $db->query($query); 
 		
 	}
+	else if($cmd == "upvote")
+	{
+		//WARNING: Authorization check not implemented!!
+		//WARNING: Amount of upvotes not tracked!
+		$query = "CALL UpVoteAnswer($answer_id)";
+		$db->query($query);
+		
+	}
+	else if($cmd == "downvote")
+	{
+		//WARNING: Authorization check not implemented!!
+		//WARNING: Amount of upvotes not tracked!
+		$query = "CALL DownVoteAnswer($answer_id)";
+		$res = $db->query($query); 
+	}
+	
 ?>
