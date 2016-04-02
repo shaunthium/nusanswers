402a34b4f0c3032ac82196c017d9fe5d30f45df0
BlackRage83 <alejandrolealislaflores@hotmail.com>
2016-04-02 23:27:50 +0800

    Added navbar-controller. The navbar is always visible in the website. The navbar controller should control:     + searchfield     + searchfield popup     + notifications     + profile button     + home button

---
+++ client/js/controllers/navbar-controller.js
@@ -0,0 +1,13 @@
+/*
+    TODO: implement the navbar controller.
+    The navbar controller should control:
+        + searchfield
+        + searchfield popup
+        + notifications
+        + profile button
+        + home button
+*/
+angular.module('quoraApp')
+.controller('NavCtrl', [ '$scope', '$rootScope', '$state', function($scope, $rootScope, $state){
+
+}]);
