faa4b812c02406070c0e01fdd0236bdbbd0223b2
Shaun Thium <shaun.thium@gmail.com>
2016-04-03 00:13:39 +0800

    Refactor questions.php to use updated method of getting data from Angular HTTP request.

--- server/questions.php
+++ server/questions.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 	require_once 'connect.php';
-	if(isset($_POST["cmd"])){
-		$cmd = $_POST["cmd"];
-	}
-	
-	//Submit new questions
-	if($cmd == "new_qn"){
-		$user_id= $_POST["user_id"];
-		$title = $db->escape_string($_POST["title"]);
-		$content= $db->escape_string($_POST["content"]);
+  $request_data = file_get_contents("php://input");
+  $data = json_decode($request_data);
+  $cmd = $data->cmd;
 
-		if(empty($title) || empty($content)){
+	//Submit new questions
+	if ($cmd == "new_qn") {
+    $user_id = $data->user_id;
+    $title = $db->escape_string($data->title);
+    $content= $db->escape_string($data->content);
+
+		if (empty($title) || empty($content)) {
 			exit("Empty title or content");
 		}
 
-		$query = "INSERT INTO Questions(user_id, title, content, score, view_count) 
+		$query = "INSERT INTO Questions(user_id, title, content, score, view_count)
 			VALUES(".$user_id.",'".$title."','".$content."', 0, 0)";
 
 		$db->query($query);
@@ -30,23 +30,23 @@
 
 
 	///Get all Title & content from "Questions" database
-	if($cmd == "qns_title"){
+	if ($cmd == "qns_title") {
 		$query = "SELECT title, content FROM Questions";
 		$result = $db->query($query);
 		$title_array = array();
 		while ($title = mysqli_fetch_assoc($result)){
 			$title_array[] = $title;
 		}
-		echo json_encode($title_array);		
+		echo json_encode($title_array);
 	}
 
 	//Get Trending post. The post is sorted in descending order of the Score of each post
-	if($cmd == "trending"){
+	if ($cmd == "trending") {
 		$query = "SELECT * FROM Questions ORDER BY score DESC";
 		$result = $db->query($query);
 		$post_array = array();
 		while ($post = mysqli_fetch_array($result)){
-					
+
 			//$post_array[] = array(
 			$post_array[] = array(
 				'id'=>$post['id'],
@@ -59,47 +59,47 @@
 				'updated_at'=>$post['updated_at']
 			);
 		}
-		echo json_encode($post_array);		
+		echo json_encode($post_array);
 	}
 
 	//Up Vote for Questions
-	if($cmd == "qns_upvote"){
-		$id= $_POST["id"];
+	if ($cmd == "qns_upvote") {
+    $id = $data->id;
 		$query = "UPDATE Questions SET score = score + 1 WHERE id=" . $id;
 		$db->query($query);
 		/*
 		if($db->query($query)){
 			echo "id='" . $id . "' Up Voted";
 		}else{
-			echo "id='" . $id . "' Fail to up vote";		
+			echo "id='" . $id . "' Fail to up vote";
 		}
 		*/
 	}
 
 	//Down Vote for Questions
-	if($cmd == "qns_downvote"){
-		$id= $_POST["id"];
+	if ($cmd == "qns_downvote") {
+    $id = $data->id;
 		$query = "UPDATE Questions SET score = score - 1 WHERE id=" . $id;
 		$db->query($query);
 		/*
 		if($db->query($query)){
 			echo "id='" . $id . "' Down Voted";
 		}else{
-			echo "id='" . $id . "' Fail to down vote";		
+			echo "id='" . $id . "' Fail to down vote";
 		}
 		*/
 	}
 
 	//View Count for Visitors Viewing the Questions every session
 	if($cmd == "qns_view_count"){
-		$id= $_POST["id"];
+    $id = $data->id;
 		$query = "UPDATE Questions SET view_count = view_count + 1 WHERE id=" . $id;
 		$db->query($query);
 		/*
 		if($db->query($query)){
 			echo "id='" . $id . "' Viewed";
 		}else{
-			echo "id='" . $id . "' Fail to view";		
+			echo "id='" . $id . "' Fail to view";
 		}
 		*/
 	}
@@ -107,13 +107,13 @@
 	//////////////////////////////////////////////////////////////////////////////////////////////////////
 	/* Code meant for internal testing only */
 	//////////////////////////////////////////////////////////////////////////////////////////////////////
-	/* 
+	/*
 	For testing purpose only
 	Update 'score' of user using 'id'
 	*/
 	if($cmd == "update_score"){
-		$id= $_POST["id"];
-		$score = $_POST["score"];
+    $id = $data->id;
+    $score = $data->score;
 		$query = "UPDATE Questions SET score=". $score . " WHERE id=" . $id;
 		if($db->query($query)){
 			echo "Score Updated";
@@ -122,13 +122,13 @@
 		}
 	}
 
-	/* 
+	/*
 	For testing purpose only
 	Update 'view_count' of user using 'id'
 	*/
 	if($cmd == "update_view"){
-		$id= $_POST["id"];
-		$view_count = $_POST["view_count"];
+    $id = $data->id;
+    $view_count = $data->view_count;
 		$query = "UPDATE Questions SET view_count=". $view_count . " WHERE id=" . $id;
 		if($db->query($query)){
 			echo "View_Count Updated";
@@ -137,12 +137,12 @@
 		}
 	}
 
-	/* 
+	/*
 	For testing purpose only
 	Delete row from table
 	*/
 	if($cmd == "delete"){
-		$id= $_POST["id"];
+    $id = $data->id;
 		$query = "DELETE FROM Questions WHERE id=" . $id;
 		if($db->query($query)){
 			echo "Row with id='" . $id . "' had been deleted";
