c6e3d54cec33831a3dbdb16233fe6ad5053bd3e6
Michael Novén <michael.f.noven@gmail.com>
2016-04-04 22:50:11 +0800

    commented and cleaned some

--- js/controllers/profile-controller.js
+++ js/controllers/profile-controller.js
@@ -1,30 +1,21 @@
-/*Controls the details of the posts */
-
 angular.module('quoraApp')
 .controller('ProfileCtrl', [ '$scope', function($scope){
 	
 	$scope.profileImg = "http://dummyimage.com/300/09.png/fff"
-
-
+ 
+	/** listens for change event on the input file type 
+	    TODO: might want to change this to a directive or something*/
 	$("input:file").change(function (){
-			console.log("lol")
      	var fileName = $(this).val();
        
       var f = document.getElementById('file-input').files[0],
       r = new FileReader();
 	  	r.onloadend = function(e){
 		    var data = e.target.result;
-		    console.log("data", data);
-
-			// Update database here
 				$scope.profileImg = data;
-				$scope.$apply();
-
+				$scope.$apply(); // update view
 		  }
 
 		  r.readAsDataURL(f);
-
   });
-
-
 }])
