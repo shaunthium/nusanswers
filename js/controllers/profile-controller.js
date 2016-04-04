823aa99c73be0b2417927585b1d2686e52e7229c
Michael Novén <michael.f.noven@gmail.com>
2016-04-04 22:45:30 +0800

    add user profile with basic image upload

--- js/controllers/profile-controller.js
+++ js/controllers/profile-controller.js
@@ -2,8 +2,29 @@
 
 angular.module('quoraApp')
 .controller('ProfileCtrl', [ '$scope', function($scope){
-		
-	console.log("in profile controller")
+	
+	$scope.profileImg = "http://dummyimage.com/300/09.png/fff"
+
+
+	$("input:file").change(function (){
+			console.log("lol")
+     	var fileName = $(this).val();
+       
+      var f = document.getElementById('file-input').files[0],
+      r = new FileReader();
+	  	r.onloadend = function(e){
+		    var data = e.target.result;
+		    console.log("data", data);
+
+			// Update database here
+				$scope.profileImg = data;
+				$scope.$apply();
+
+		  }
+
+		  r.readAsDataURL(f);
+
+  });
 
 
 }])
