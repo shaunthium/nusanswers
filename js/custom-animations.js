ea89d800d7032f19f586f83217b4913249a545af
Michael Novén <michael.f.noven@gmail.com>
2016-04-06 15:50:40 +0800

    add transitions on overlay container and feed view

---
+++ js/custom-animations.js
@@ -0,0 +1,25 @@
+angular.module('quoraApp')
+.animation('.slide', [function() {
+  return {
+    // make note that other events (like addClass/removeClass)
+    // have different function input parameters
+    enter: function(element, doneFn) {
+
+      console.log("entering")
+      jQuery(element).fadeIn(500, doneFn);
+
+      // remember to call doneFn so that angular
+      // knows that the animation has concluded
+    },
+
+    move: function(element, doneFn) {
+      console.log("moving")
+      jQuery(element).fadeIn(500, doneFn);
+    },
+
+    leave: function(element, doneFn) {
+      console.log("leaving")
+      jQuery(element).fadeOut(500, doneFn);
+    }
+  }
+}]);
