4f7677149dcf708c59f01d8d9e9055f6a959b891
Shaun Thium <shaun.thium@gmail.com>
2016-04-07 17:13:39 +0800

    More integration.

--- js/controllers/profile-controller.js
+++ js/controllers/profile-controller.js
@@ -1,23 +1,27 @@
 angular.module('quoraApp')
 .controller('ProfileCtrl', [ '$scope', '$http', function($scope, $http){
-  FB.getLoginStatus(function(resp) {
-    if (resp.status == 'connected') {
-      FB.api('/me', function(response) {
-        // User's Facebook name
-        $scope.userName = response.name;
-        // Get user's profile picture
-        $http({
-          url: 'http://graph.facebook.com/v2.5/' + response.id + '/picture?redirect=false',
-          method: 'GET',
-          data: {
-            width: '1000'
-          }
-        }).success(function(data) {
-          $scope.profileImg = data.data.url;
-        });
+  // For temporary use only while fb integration is not complete
+  $scope.userName = 'CS3226';
+  $scope.profileImg = "http://dummyimage.com/300/09.png/fff";
 
-        $scope.$apply();
-      });
-    }
-  });
+  // FB.getLoginStatus(function(resp) {
+  //   if (resp.status == 'connected') {
+  //     FB.api('/me', function(response) {
+  //       // User's Facebook name
+  //       $scope.userName = response.name;
+  //       // Get user's profile picture
+  //       $http({
+  //         url: 'http://graph.facebook.com/v2.5/' + response.id + '/picture?redirect=false',
+  //         method: 'GET',
+  //         data: {
+  //           width: '1000'
+  //         }
+  //       }).success(function(data) {
+  //         $scope.profileImg = data.data.url;
+  //       });
+  //
+  //       $scope.$apply();
+  //     });
+  //   }
+  // });
 }])
