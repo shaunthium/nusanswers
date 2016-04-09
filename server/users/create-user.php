bf9792277243a81792fe05b6196f7020d32ea500
CS3226 <cs3226@nusanswers.com>
2016-04-09 22:56:03 +0800

    Fix bug with create-user.php. Add check for adding user to db if not exist.

--- server/users/create-user.php
+++ server/users/create-user.php
@@ -7,7 +7,7 @@
   $accessToken = $_POST["token"];
   try {
     // Returns a `Facebook\FacebookResponse` object
-    $response = $fb->api('/me', $accessToken);
+    $response = $fb->get('/me', $accessToken);
   } catch(Facebook\Exceptions\FacebookResponseException $e) {
     echo 'Graph returned an error: ' . $e->getMessage();
     exit;
@@ -17,8 +17,13 @@
   }
 
   $user = $response->getGraphUser();
-  print_r(error_log($user), true);
 
-  // $query = "INSERT INTO Users (id) VALUES (" . intval($user["id"]) . ")";
-  // $db->query($query);
+  // $check_query = "SELECT id FROM Users WHERE id=" . $user["id"];
+  $check_query = "SELECT id FROM Users WHERE id=" . 1000;
+  $check_result = $db->query($check_query);
+  if (mysqli_fetch_row($check_result) == null) {
+    // Does not exist yet, create user
+    $query = "INSERT INTO Users (id) VALUES (" . intval($user["id"]) . ")";
+    $db->query($query);
+  }
  ?>
