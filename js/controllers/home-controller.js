8ea246e6429faf99794ed00fc1c7b1e6f0d6241b
BlackRage83 <alejandrolealislaflores@hotmail.com>
2016-04-14 22:19:00 +0800

    Fixed home view tag filter bug.

--- js/controllers/home-controller.js
+++ js/controllers/home-controller.js
@@ -15,8 +15,7 @@ angular.module('quoraApp')
         }
         return posts.filter(function(post){
             var relevant = false;
-            if(!post.tags || typeof post.tags === 'undefined' || post.tags.length === 0)
-                return;
+            if(!(!post.tags || typeof post.tags === 'undefined' || post.tags.length === 0))
             post.tags.forEach(function(postTag){
                 tags.forEach(function(arrayTag){
                     relevant = relevant || postTag.toUpperCase() === arrayTag.toUpperCase();
@@ -29,6 +28,14 @@ angular.module('quoraApp')
 })
 .controller('HomeCtrl', [ '$scope', '$stateParams', 'byTagsFilter', function($scope, $stateParams, filterByTags){
     $scope.activeTags = [];
+
+    //Watch for changes in activeTags, coming from the trending-box
+    $scope.$watchCollection(function(){
+        return $scope.posts;
+    },
+    function(newPosts){
+        $scope.filteredPosts = filterByTags(newPosts, $scope.tags);
+    });
 
     //Watch for changes in activeTags, coming from the trending-box
     $scope.$watchCollection(function(){
