c4fdccbedd7b8f000da9915d5f99c046e2ce9c96
Michael Novén <michael.f.noven@gmail.com>
2016-04-15 16:34:09 +0800

    user can't answer, upvote and stuff when not logged in

--- js/directives/post-directive.js
+++ js/directives/post-directive.js
@@ -72,12 +72,21 @@ angular.module('quoraApp')
             }*/
 
             $scope.toggleFooter = function(){
+
+                if(!$scope.currentUser){
+                  $scope.showLogin();
+                  return;
+                }
+
                 $scope.showFooter = !$scope.showFooter;
             }
 
             $scope.incrementUpvotes = function(post, inc) {
 
-              console.log("incrementing upvotes on " , post.id );
+              if(!$scope.currentUser){
+                $scope.showLogin();
+                return;
+              }
 
               // User should not be able to downvote/upvote multiple times
               // change loggedinUserId to $scope.facebookuserid or something..
