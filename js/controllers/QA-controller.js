04186b330d8c3dbaae8804d67a0642975355a397
BlackRage83 <alejandrolealislaflores@hotmail.com>
2016-04-16 12:19:48 +0800

    Infinite feed and questions URL. Implemented infinite feed. Fixed a bug where loading screen never resolved when accessing a question directly through its ID.

--- js/controllers/QA-controller.js
+++ js/controllers/QA-controller.js
@@ -10,30 +10,5 @@ angular.module('quoraApp')
       $scope.userID = undefined;
   }
 
-  qs.getPost($stateParams.questionId, $scope.userID)
-    .then(function(res){
-      $scope.post = res.data.question;
-      if(!$scope.post){
-        // console.log("NO POST IN DB, SHOW 404 NOT FOUND ");
-      } else {
-        qs.getAnswersToCurrentPost($scope.post.id)
-        .then(function(res){
-
-          $scope.post.answers = res.data.answers;
-          if($scope.post.answers.length > 0)
-            $scope.numAnswers = $scope.post.answers.length;
-          else
-            $scope.numAnswers = 0;
-
-        }, function(err){
-
-          // console.log("err in getting answers to post", err);
-
-        });
-      }
-    }, function(err){
-
-      // console.log("err in getting post with id " + $stateParams.questionId, err);
-
-    });
+  $scope.getPost($stateParams.questionId, $scope.userID);
 }]);
