b8de1ae196098eda1ea7675b33f9d099addf97c0
Shaun Thium <shaunthium@users.noreply.github.com>
2017-07-09 21:16:49 -0400

    Update fb.php

--- fb.php
+++ fb.php
@@ -1,7 +1,7 @@
 <?php   require_once __DIR__ . '/vendor/autoload.php';
     $fb = new Facebook\Facebook([
-    'app_id' => '1616624788586225',
-    'app_secret' => 'd39f2559fada2fcdf511203f1d6a6f63',
+    'app_id' => '<app id>',
+    'app_secret' => '<app secret>',
     'default_graph_version' => 'v2.5',
   ]);
 ?>
